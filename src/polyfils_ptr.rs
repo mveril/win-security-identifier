b28b95777790c4eb9cd1694bb3721b37c022cda7
Mickaël Véril <mika.veril@wanadoo.fr>
2025-10-18 19:23:05 +0200

    Fix lits for stable polifyls

--- src/polyfils_ptr.rs
+++ src/polyfils_ptr.rs
@@ -1,16 +1,19 @@
 use crate::sid::Sid;
 
 #[allow(dead_code)]
-pub(super) const unsafe fn from_raw_parts_mut(src: *mut (), dynamic_size_count: usize) -> *mut Sid {
+#[allow(clippy::cast_ptr_alignment)]
+pub const fn from_raw_parts_mut(src: *mut (), dynamic_size_count: usize) -> *mut Sid {
     core::ptr::slice_from_raw_parts_mut(src, dynamic_size_count) as *mut Sid
 }
 
-pub(super) const unsafe fn from_raw_parts(src: *const (), dynamic_size_count: usize) -> *mut Sid {
-    core::ptr::slice_from_raw_parts(src, dynamic_size_count) as *mut Sid
+#[allow(clippy::cast_ptr_alignment)]
+pub const fn from_raw_parts(src: *const (), dynamic_size_count: usize) -> *const Sid {
+    core::ptr::slice_from_raw_parts(src, dynamic_size_count) as *const Sid
 }
 
 #[allow(dead_code)]
-pub(super) const fn metadata(sid: *const Sid) -> usize {
+pub const fn metadata(sid: *const Sid) -> usize {
+    // SAFETY: as Sid is a fat pointer it's safe to use
     unsafe {
         let slice = &*(sid as *const [()]);
         slice.len()
