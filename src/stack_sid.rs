03b16cbc5e6d6dc04938ecd5051df75e496c0bf2
Mickaël Véril <mika.veril@wanadoo.fr>
2025-11-29 16:52:26 +0100

    Fix StackSid Clone and debug (MaybeUninit bug) (#15)

--- src/stack_sid.rs
+++ src/stack_sid.rs
@@ -7,15 +7,14 @@ use core::ptr::{from_raw_parts, from_raw_parts_mut};
 use crate::sid::MAX_SUBAUTHORITY_COUNT;
 use crate::utils::{sub_authority_size_guard, validate_sid_bytes_unaligned};
 use crate::{Sid, SidIdentifierAuthority};
-use core::fmt::Display;
-use core::mem::MaybeUninit;
-use core::ptr::{self, copy_nonoverlapping};
+use core::fmt::{self, Display};
+use core::mem::{MaybeUninit, size_of, size_of_val};
+use core::ptr;
 use core::str::FromStr;
 use delegate::delegate;
 use parsing::{self, InvalidSidFormat};
 
 #[repr(C)]
-#[derive(Debug, Clone, Copy)]
 pub struct StackSid {
     /// The SID revision value, generally 1.
     pub revision: u8,
@@ -100,7 +99,7 @@ impl StackSid {
         let array_ptr: *mut u32 = array.as_mut_ptr().cast();
         // Safety: We already check the length of sub_authority to be in 1..=15.
         unsafe {
-            copy_nonoverlapping(sub_authority.as_ptr(), array_ptr, sub_authority.len());
+            array_ptr.copy_from_nonoverlapping(sub_authority.as_ptr(), sub_authority.len());
         }
 
         Self {
@@ -134,7 +133,7 @@ impl StackSid {
     /// This allows treating the fixed-size `StackSid` as a regular `Sid`
     /// with a trailing slice of sub-authorities.
     #[inline]
-    pub fn as_sid_mut(&mut self) -> &mut Sid {
+    pub const fn as_sid_mut(&mut self) -> &mut Sid {
         let raw: *mut () = ptr::from_mut(self).cast();
         // SAFETY: same justification as `as_sid`, but for a mutable reference.
         unsafe { &mut *from_raw_parts_mut(raw, self.sub_authority_count as usize) }
@@ -148,6 +147,20 @@ impl StackSid {
             #[must_use]
             #[inline]
             pub const fn as_binary(&self) -> &[u8];
+        }
+
+        to self.as_sid_mut() {
+            /// Returns a `&mut [u8]` view over the **currently valid** minimal binary representation.
+            ///
+            /// This can be used for low-level, in-place updates when you know exactly what you are doing.
+            ///
+            /// # Safety
+            /// - Same preconditions as `as_binary`.
+            /// - Mutating the buffer must preserve SID invariants (e.g., do not desynchronize
+            ///   `sub_authority_count` and the tail length).
+            #[must_use]
+            #[inline]
+            pub const unsafe fn as_binary_mut(&mut self) -> &mut [u8];
         }
     }
 
@@ -183,11 +196,49 @@ impl StackSid {
         let mut sid = MaybeUninit::<Self>::uninit();
         // SAFETY: `StackSid` has max size of `Sid`
         unsafe {
-            ptr::copy_nonoverlapping(bytes.as_ptr(), sid.as_mut_ptr().cast::<u8>(), bytes.len());
+            sid.as_mut_ptr()
+                .cast::<u8>()
+                .copy_from_nonoverlapping(bytes.as_ptr(), bytes.len());
         }
         // SAFETY: Initialized by previous
         let sid = unsafe { sid.assume_init() };
         Ok(sid)
+    }
+}
+
+impl fmt::Debug for StackSid {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct(stringify!(StackSid))
+            .field(stringify!(revision), &self.revision)
+            .field(stringify!(sub_authority_count), &self.sub_authority_count)
+            .field(stringify!(identifier_authority), &self.identifier_authority)
+            .field(stringify!(sub_authority), &self.get_sub_authorities())
+            .finish()
+    }
+}
+
+impl Clone for StackSid {
+    #[inline]
+    fn clone(&self) -> Self {
+        self.as_sid().into()
+    }
+
+    #[inline]
+    fn clone_from(&mut self, source: &Self) {
+        // Safety: Binary copy from another stackSid is safe
+        let binary_source = source.as_binary();
+        debug_assert!(
+            binary_source.len() <= size_of::<Self>(),
+            "StackSid Size should be max size of Sid"
+        );
+        let len = binary_source.len();
+        // SAFETY: Preconditon checked with debug_assert!
+        unsafe {
+            ptr::from_mut(self)
+                .cast::<u8>()
+                .copy_from(binary_source.as_ptr(), len);
+        }
     }
 }
 
@@ -218,13 +269,16 @@ impl From<&Sid> for StackSid {
     #[inline]
     fn from(value: &Sid) -> Self {
         let mut uninit = MaybeUninit::<Self>::uninit();
+        let binary_source = value.as_binary();
+        let len = binary_source.len();
         let mem = uninit.as_mut_ptr().cast::<u8>();
-        // SAFETY: We know result is bigger than input
+        debug_assert!(
+            size_of_val(value) <= size_of::<Self>(),
+            "StackSid Size should be max size of Sid, it's not true for this value"
+        );
+        // SAFETY: precondition checked with debug_assert!
         unsafe {
-            mem.copy_from_nonoverlapping(
-                ptr::from_ref(value).cast(),
-                value.get_current_min_layout().size(),
-            );
+            mem.copy_from_nonoverlapping(binary_source.as_ptr(), len);
         }
         // SAFETY: Initialized by the previous step
         unsafe { uninit.assume_init() }
@@ -268,10 +322,14 @@ impl PartialEq for StackSid {
     }
 }
 
+impl Eq for StackSid {}
+
 impl Hash for StackSid {
-    #[inline]
-    fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
-        self.as_sid().hash(state);
+    delegate! {
+        to self.as_sid() {
+            #[inline]
+            fn hash<H: core::hash::Hasher>(&self, state: &mut H);
+        }
     }
 }
 
@@ -286,5 +344,72 @@ impl PartialEq<StackSid> for Sid {
     #[inline]
     fn eq(&self, other: &StackSid) -> bool {
         self.eq(other.as_sid())
+    }
+}
+
+#[cfg(test)]
+#[allow(clippy::unwrap_used, clippy::expect_used)]
+mod tests {
+    use super::*;
+    use crate::arb_identifier_authority;
+    #[cfg(not(has_ptr_metadata))]
+    use crate::polyfills_ptr::metadata;
+    #[cfg(has_ptr_metadata)]
+    use core::ptr::metadata;
+    use proptest::prelude::*;
+    pub fn arb_stack_sid() -> impl Strategy<Value = StackSid> {
+        (
+            Just(1u8), // revision
+            arb_identifier_authority(),
+            proptest::collection::vec(any::<u32>(), 1..=15),
+        )
+            .prop_map(|(revision, identifier_authority, sub_authorities)| {
+                let subs = &sub_authorities.as_slice();
+                StackSid::try_new(revision, identifier_authority, subs)
+                    .expect("Failed to generate StackSid")
+            })
+    }
+    #[test]
+    fn debug_output_is_exact() {
+        let sid = StackSid::try_new(
+            1,
+            SidIdentifierAuthority::NT_AUTHORITY,
+            &[21u32, 42u32, 1337u32],
+        )
+        .unwrap();
+
+        let actual = format!("{sid:?}");
+
+        let expected = "StackSid { revision: 1, sub_authority_count: 3, identifier_authority: SidIdentifierAuthority { value: [0, 0, 0, 0, 0, 5] }, sub_authority: [21, 42, 1337] }";
+        assert_eq!(actual, expected);
+    }
+
+    proptest! {
+        #[test]
+        fn test_stack_sid_clone(sid in arb_stack_sid()){
+            prop_assert_eq!(sid.clone(), sid);
+
+        }
+
+        #[test]
+        fn test_stack_sid_clone_from(mut sid in arb_stack_sid(), sid_source in arb_stack_sid()){
+            sid.clone_from(&sid_source);
+            prop_assert_eq!(sid, sid_source);
+        }
+
+        #[test]
+        fn test_as_sid_mut(mut sid in arb_stack_sid()){
+            let self_addr =ptr::from_mut(&mut sid).addr();
+            let sid_ref = sid.as_sid_mut();
+            prop_assert_eq!(ptr::from_mut(sid_ref).addr(), self_addr);
+            prop_assert_eq!(metadata(sid_ref), sid.sub_authority_count as usize);
+        }
+
+        #[test]
+        fn test_as_sid(sid in arb_stack_sid()){
+            let sid_ref = sid.as_sid();
+            prop_assert_eq!(ptr::from_ref(sid_ref).addr(), ptr::from_ref(&sid).addr());
+            prop_assert_eq!(metadata(sid_ref), sid.sub_authority_count as usize);
+        }
     }
 }
