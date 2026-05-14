4507d19d3980ac57464406117a05624a341ec151
Mickaël Véril <mika.veril@wanadoo.fr>
2026-05-14 22:25:14 +0200

    Use successful lookup for SID type (#31)

--- tests/current_sid.rs
+++ tests/current_sid.rs
@@ -3,7 +3,6 @@
 #![cfg(feature = "std")]
 #![cfg(feature = "serde")]
 #![allow(clippy::expect_used, reason = "Expect is not an issue in tests")]
-#![allow(clippy::unwrap_used, reason = "Unwrap is not an issue in tests")]
 #![allow(clippy::std_instead_of_core)]
 
 use serde::Deserialize;
@@ -77,21 +76,51 @@ where
 
     assert_eq!(sid, user.sid, "SID does not match expected value");
 
-    let lookup = sid.as_ref().lookup_local_sid().unwrap().unwrap();
+    let lookup = sid
+        .as_ref()
+        .lookup_local_sid()
+        .map(|lookup| {
+            lookup
+                .map(|lookup| {
+                    let sid_type = lookup.sid_type();
+                    (lookup.domain_name, sid_type)
+                })
+                .map_err(|_| ())
+        })
+        .map(|lookup| {
+            lookup.map(|(domain_name, sid_type)| (domain_name, sid_type.map_err(|_| ())))
+        });
 
     assert_eq!(
-        lookup.domain_name, user.account,
+        lookup,
+        Some(Ok((user.account, Ok(SidType::User)))),
         "Domain and name do not match expected value"
     );
 
+    let lookup_sid_type = lookup.as_ref().map(|lookup| match lookup {
+        Ok((_, sid_type)) => *sid_type,
+        Err(()) => Err(()),
+    });
+
     assert_eq!(
-        lookup.sid_type().unwrap(),
-        SidType::User,
+        lookup_sid_type,
+        Some(Ok(SidType::User)),
         "Domain and name do not match expected value"
     );
+
+    let local_sid_type = sid
+        .as_ref()
+        .local_sid_type()
+        .map(|sid_type| sid_type.map_err(|_| ()));
+
     assert_eq!(
-        lookup.sid_type().unwrap(),
-        SidType::User,
-        "Domain and name do not match expected value"
+        local_sid_type,
+        Some(Ok(SidType::User)),
+        "Local SID type does not match expected value"
+    );
+
+    assert_eq!(
+        local_sid_type, lookup_sid_type,
+        "Local SID type should match lookup result"
     );
 }
