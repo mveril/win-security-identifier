5c66401165b603bd4f8f9d34f2fa37d50f02355a
Mickaël Véril <mika.veril@wanadoo.fr>
2026-05-14 19:17:07 +0200

    Remove unaligned reading

--- src/ext/windows/get_current_sid.rs
+++ src/ext/windows/get_current_sid.rs
@@ -121,10 +121,8 @@ where
             let err = unsafe { GetLastError() };
             return Err(TokenError::GetTokenInfoFailed(err));
         }
-        // SAFETY: TOKEN_USER is a plain data struct and can be read from a byte buffer.
-        let sid_ptr = unsafe { ptr::addr_of!((*token_user_ptr).User.Sid) };
         // SAFETY: TOKEN_USER contains a PSID which is a pointer to a valid SID.
-        let raw_sid: *mut core::ffi::c_void = unsafe { ptr::read_unaligned(sid_ptr) };
+        let raw_sid = unsafe { (*token_user_ptr).User.Sid };
         // SAFETY: get the user Sid from the raw pointer structure.
         let sid = unsafe { Sid::from_raw(raw_sid) };
         Ok(sid.into())
